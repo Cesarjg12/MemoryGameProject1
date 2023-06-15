3fcbd216b8e17d32e6765b6b47702e27bcc57ed1
Cesarjg12 <136657517+Cesarjg12@users.noreply.github.com>
2023-06-15 16:00:19 -0700

    Update main.js

---
+++ main.js
@@ -0,0 +1,56 @@
+/*----- constants -----*/
+
+//const for cards and their win and null option.
+
+const cards = {
+  '1': //card pair matches
+  'null': //card not matching pair
+}
+//Const for when card 1 a matches card 2 a, card 3 b matches card 4 b. 
+const wins [
+  [1a = 2a],
+  [3b = 4b]
+]
+
+
+
+/*----- state variables -----*/
+//this allows the player to interact with board, get a result back and return with a matching pair.
+let board, turn, winner;
+
+
+/*----- cached elements  -----*/
+//This will use our variables h1 and button to show the text of h1 and button.
+
+const messageWin = document.querySelector('h1');
+const playAgain = document.querySelector('button');
+
+
+/*----- event listeners -----*/
+
+//To grab our board and make it have a 'click'
+//To add an eventlistener on our button
+document.getElementById('board').addEventListener('click', playerTurn)
+playAgain.addEventListener('click',init)
+
+
+
+
+/*----- functions -----*/
+
+
+
+
+
+
+//Winner function: 
+
+//Render a function to get win or have the cards go back, and allow player to reinitialize matching cards.
+
+function renderMessage () {
+    if (winner === 'T') {
+        message.innerHTML = '';
+    } else if (winner) {
+        message.innerHTML = '';
+    }
+}
