a0e9a3129479ac1277e138cb6b160d55ba319a15
cesarjg28 <cesarjg12@yahoo.com>
2023-06-20 15:07:00 -0700

    yes

--- main.js
+++ main.js
@@ -1,64 +1,63 @@
-// /*----- constants -----*/
+/*----- constants -----*/
 
-// //const for cards and their win and null option.
+//const for cards and their win and null option.
 
-// const cards = {
-//   '1': //card pair matches
-//   'null': //card not matching pair
-// }
-// //Const for when card 1 a matches card 2 a, card 3 b matches card 4 b. 
-// const wins [
-//   [1a = 2a],
-//   [3b = 4b]
-// ]
-
-
-
-// /*----- state variables -----*/
-// //this allows the player to interact with board, get a result back and return with a matching pair.
-// let board, turn, winner;
-
-
-// /*----- cached elements  -----*/
-// //This will use our variables h1 and button to show the text of h1 and button.
-
-// const messageWin = document.querySelector('h1');
-// const playAgain = document.querySelector('button');
-
-
-// /*----- event listeners -----*/
-
-// //To grab our board and make it have a 'click'
-// //To add an eventlistener on our button
-// document.getElementById('board').addEventListener('click', playerTurn)
-// playAgain.addEventListener('click',init)
+const cards = {
+  '1': //card pair matches
+  'null': //card not matching pair
+}
+//Const for when card 1 a matches card 2 a, card 3 b matches card 4 b. 
+const wins [
+  [1a = 2a],
+  [3b = 4b]
+]
 
 
 
+/*----- state variables -----*/
+//this allows the player to interact with board, get a result back and return with a matching pair.
+let board, turn, winner;
 
-// /*----- functions -----*/
 
-// function pickCardWinner() {
-//   for () {
-//     if ()
-//       return();
+/*----- cached elements  -----*/
+//This will use our variables h1 and button to show the text of h1 and button.
+
+const messageWin = document.querySelector('.cards');
+const playAgain = document.querySelector('button');
+const cardChooser = document.querySelectorAll('.card');
+
+/*----- event listeners -----*/
+
+//To grab our board and make it have a 'click'
+//To add an eventlistener on our button
+document.getElementById('board').addEventListener('click', playerTurn)
+playAgain.addEventListener('click', init)
+
+
+
+/*----- functions -----*/
+
+function pickCardWinner() {
+  for () {
+    if ()
+      return();
        
-//   } else () {
+  } else () {
   
-//   } return();
-//   });
+  } return();
+  });
 
 
 
 
-// //Winner function: 
+//Winner function: 
 
-// //Render a function to show result if  win or have the cards go back, and allow player to reinitialize matching cards.
+//Render a function to show result if  win or have the cards go back, and allow player to reinitialize matching cards.
 
-// function renderMessage () {
-//     if (winner === 'T') {
-//         message.innerHTML = '';
-//     } else if (winner) {
-//         message.innerHTML = '';
-//     }
-// }
+function renderMessage () {
+    if (winner === 'T') {
+        message.innerHTML = '';
+    } else if (winner) {
+        message.innerHTML = '';
+    }
+}
