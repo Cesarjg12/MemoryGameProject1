f4746c7bbfe6cdfcd5da9a920e5804e0caa8e3e1
cesarjg28 <cesarjg12@yahoo.com>
2023-06-22 14:46:08 -0700

    Added JS and readded html in the div

--- main.js
+++ main.js
@@ -22,7 +22,8 @@ const button = document.querySelector(".btn")
 button.addEventListener('click', ()=> {
   console.log('button clicked');
 })
-cardChooser.forEach(cards = cards.addEventListener('click', flipCard));
+
+cardChooser.forEach(cards => cards.addEventListener('click', flipCard));
 
 
 
@@ -37,26 +38,28 @@ function init() {
   cardChooser.forEach(cards => {
     cards.classList.remove('flipped', 'matched')  // want to have flip and match cards removed after restarting game.);
     });
-  cardShuffle();  //want to add a card shuffle
+  // cardShuffle();  //want to add a card shuffle
 }
 
 
-function cardShuffle() {
-  //add a for loop to make a card shuffle use math.floor and math random
-  for(let i = cardChooser.length - 1; i > 0, i--;) {
-    const shuffle = math.floor(math.random() * (i + 1));
-  }
-  return i
-}
+// function cardShuffle() {
+//   //add a for loop to make a card shuffle use math.floor and math random
+//   for(let i = cardChooser.length - 1; i > 0, i--;) {
+//     const shuffle = math.floor(math.random() * (i + 1));
+//   }
+//   return i
+// }
 
 
 //Add a flip card function
-function flipCard() {
-  if (cards.classList.contains('flipped') || matchedCards.includes(cards)) {
+function flipCard(event) {
+  console.log(event.target)
+  const clickedCard = event.target
+  if (clickedCard.classList.contains('flipped') || matchedCards.includes(clickedCard)) {
     return;
   }
-  highlight(cards);
-  flippedCards.push(cards)
+  // highlight(cards);
+  flippedCards.push(clickedCard)
 
   if (flippedCards.length === 2) {
     checkMatch()
@@ -69,21 +72,21 @@ const highlight = (cards) => {
 };
 
 //Add a match function for when cards match to their pair.
-function matchedCards() {
-  const [card1, card2] = flippedCards;
-  const card1 = card1.getAttribute('data-card'); //to pull the cards "data-card=()"
-  const card2 = card2.getAttribute('data-card');
-
+function checkMatch() {
+  let [card1, card2] = flippedCards;
+  card1 = card1.getAttribute('data-card'); //to pull the cards "data-card=()"
+  card2 = card2.getAttribute('data-card');
+console.log(flippedCards)
 //Make sures the cards are matched by the correct data-card using a classList to call upon it.
   
-if (card1 === card2) {
-    card1.classList.add('matched');
-    card2.classList.add('matched');
-    matchedCards.push(card1, card2)
-  }
-  if (matchedCards.length === cardChooser.length){
+if flippedCards = [0,1] {
+//     card1.classList.add('matched');
+//     card2.classList.add('matched');
+//     matchedCards.push(card1, card2)
+//   }
+//   if (matchedCards.length === cardChooser.length){
     
-  }
+//   }
   
 }
 
