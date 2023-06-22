e72c1840a5d3fb39915d9a1f53e99971d56c9fe7
cesarjg28 <cesarjg12@yahoo.com>
2023-06-22 11:28:54 -0700

    added code in js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 /*----- constants -----*/
 const cards = [ 'A', 'A', 'B', 'B']
-let flippedCards = [];
-let matchedCards = [];
 
 /*----- state variables -----*/
 
-
+let flippedCards = [];
+let matchedCards = [];
 
 /*----- cached elements  -----*/
 
@@ -23,7 +22,7 @@ const button = document.querySelector(".btn")
 button.addEventListener('click', ()=> {
   console.log('button clicked');
 })
-
+cardChooser.forEach(card = card.addEventListener('click', flipCard));
 
 
 
@@ -35,16 +34,19 @@ init();
 function init() {
   flippedCards = [],
   matchedCards = [],
-  (array).forEach(card => {
-    card.classList.remove(// want to have flip and match cards removed after restarting game.);
-    
-  });
+  cardChooser.forEach(card => {
+    card.classList.remove('flipped', 'matched')  // want to have flip and match cards removed after restarting game.);
+    });
   cardShuffle();  //want to add a card shuffle
 }
 
+
 function cardShuffle() {
   //add a for loop to make a card shuffle use math.floor and math random
-  for(let i =)
+  for(let i = cardChooser.length - 1; i > 0, i--) {
+    const shuffle = math.floor(math.random() * (i + 1));
+  }
+  return i
 }
 
 
@@ -55,6 +57,20 @@ function flipCard() {
 
 //Add a match function for when cards match to their pair.
 function matchedCards() {
+  const [card1, card2] = flippedCards;
+  const card1 = card1.getAttribute('data-card'); //to pull the cards "data-card=()"
+  const card2 = card2.getAttribute('data-card');
+
+//Make sures the cards are matched by the correct data-card using a classList to call upon it.
+  
+if (card1 === card2) {
+    card1.classList.add('matched');
+    card2.classList.add('matched');
+    matchedCards.push(card1, card2)
+  }
+  if (matchedCards.length === cardChooser.length){
+    
+  }
   
 }
 
