2565dcdf2e995d5f37f81163ce6dd51582196a51
cesarjg28 <cesarjg12@yahoo.com>
2023-06-23 22:07:52 -0700

    Broken code Committing to fix later trying to implement the card flip image.

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const messageWin = document.querySelector('h2');
 const playAgain = document.querySelector('.btn');
 const cardChooser = document.querySelectorAll('.card');
 const messageLost = document.querySelector('h3');
+const cardBack = document.querySelectorAll('.back-card');
 
 
 
@@ -54,13 +55,19 @@ function init() {
 
 //Add a flip card function
 function flipCard(event) {
-  console.log(event.target)
+  // console.log(event.target)
   const clickedCard = event.target;
   if (clickedCard.classList.contains('flipped') || matchedCards.includes(clickedCard)) {
     return;
   }
+const cardData = clickedCard.dataset.card;
+
+const cardBack = clickedCard.nextElementSibling;
+clickedCard.classList.add('flipped');
+cardBack.classList.add('flipped');
+
   // highlight(clickedCard);
-  flippedCards.push(clickedCard)
+  flippedCards.push(cardData)
   highlight(clickedCard);
 
   if (flippedCards.length === 2) {
@@ -78,18 +85,25 @@ cards.classList.add('flipped'); //'highlight');
 
 function checkMatch() {
   let [card1, card2] = flippedCards;
-  card1 = card1.getAttribute('data-card'); //to pull the cards "data-card=()"
-  card2 = card2.getAttribute('data-card');
+  // card1 = card1.getAttribute('data-card'); //to pull the cards "data-card=()"
+  // card2 = card2.getAttribute('data-card');
 
+  const frontCard1 = clickedCard;
+  const frontCard2 = cardBack.previousElementSibling;
 
 
 // if (flippedCards.length === 2) {
-  if (card1 === card2){
+  if (frontCard1.dataset.card === frontCard2.dataset.card) {
+  // if (card1 === card2){
+    card1.classList.remove('flipped');
+    card1.classList.add('matched');
+    card2.classList.remove('flipped');
+    card2.classList.add('matched');
     
-    flippedCards.forEach((clickedCard) => {
-      clickedCard.classList.remove('flipped');
-      clickedCard.classList.add('matched');
-    });
+    // flippedCards.forEach((clickedCard) => {
+    //   clickedCard.classList.remove('flipped');
+    //   clickedCard.classList.add('matched');
+    // });
     matchedCards.push(card1, card2);
   resetCards();
   gameEnds();
@@ -113,7 +127,6 @@ function resetCards() {
   }
 
 function gameEnds(){
-console.log('Hello')
   if (matchedCards.length === cardChooser.length) {
     const messageWin = document.querySelector('h2');
     messageWin.innerHTML = 'You matched all the pairs!';
